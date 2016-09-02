52a94a6e2b672aade342e5ec961cd061062bf103
Bharath D N <bharathmh@gmail.com>
2016-09-02 16:43:09 -0400

    Implemented QuickUnion

---
+++ Algorithms/src/QuickUnionUF.java
@@ -0,0 +1,32 @@
+
+public class QuickUnionUF {
+	
+	private int [] array;
+	
+	public QuickUnionUF(int N){
+		array = new int[N];
+	}
+	
+	public int Root(int i){
+		while(i != array[i]){
+			i = array[i];
+		}
+		return i;
+	}
+	
+	public boolean Connected(int x, int y){
+		return (Root(x) == Root(y));
+	}
+	
+	public void Union(int x, int y){
+		if(Connected(x,y))
+			return;
+		
+		int rootX = Root(x);
+		int rootY = Root(y);
+		
+		// set y's root as parent to x's root
+		array[rootX] = rootY; 
+	}
+
+}
